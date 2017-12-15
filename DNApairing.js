ad525164862868d60197c0dc6d1b9631754ed7e3
Stephanie Shea <stephcshea@gmail.com>
2017-12-15 01:15:51 +0000

    simplified DNA pairing  if statements

--- DNApairing.js
+++ DNApairing.js
@@ -1,21 +1,17 @@
 function pairElement(str) {
-	var mainArray = [];
-	var pairArray = [];
+	 var mainArray = [];
+  
 	for (var i = 0; i < str.length; i++) {
 		if (str.charAt(i) === "C") {
-			pairArray = ["C", "G"];
-			mainArray.push(pairArray);
+			mainArray.push(["C", "G"]);
 		} else if (str.charAt(i) === "G") {
-			pairArray = ["G", "C"];
-			mainArray.push(pairArray);
+			mainArray.push(["G", "C"]);
 		} else if (str.charAt(i) === "T") {
-			pairArray = ["T", "A"];
-			mainArray.push(pairArray);
+			mainArray.push(["T", "A"]);
 		} else if (str.charAt(i) === "A") {
-			pairArray = ["A", "T"];
-			mainArray.push(pairArray);
+			mainArray.push(["A", "T"]);
 		}
 	}
 	return mainArray;
 }
-pairElement("CGCTA");
+pairElement("CGCTAGCA");
