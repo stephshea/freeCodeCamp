86fcf2c3455aa9033ffaef5701e2b523a9906589
Stephanie Shea <stephcshea@gmail.com>
2017-12-16 17:26:38 +0000

    tidied spinalTap code

--- spinalTapCase.js
+++ spinalTapCase.js
@@ -1,8 +1,6 @@
 //remove _, spaces, separate joined words at cap, all letters lowercase
 function spinalCase(str) {
-
-  str = str.replace(/([a-z])([A-Z])/g,'$1 $2').replace(/\s+/g, '-').replace(/\_+/g, '-').toLowerCase();
-  return str;
+	str = str.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/\s+/g, '-').replace(/\_+/g, '-').toLowerCase();
+	return str;
 }
-
 spinalCase('isThis Spinal Tap');
