2e887e590f3ff8a4e444f9c3eaac18e7974eeadf
Stephanie Shea <stephcshea@gmail.com>
2017-12-19 03:30:47 +0000

    psuedo and a solution for sumPrimes

--- sumPrimes.js
+++ sumPrimes.js
@@ -1,17 +1,18 @@
-function sumPrimes(num) {
-	var totalPrimes = 0;
-	var primesArray = [];
-	for (var i = 2; i <= num; i++) {
-		if ((i === 2) || (i === 3) || (i === 5) || (i === 7)) {
-			primesArray.push(i);
-		}
-		if ((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) {
-			primesArray.push(i);
+function isPrime(num) {
+	for (var n = 2; n < num; n++) {
+		if (num % n == 0) {
+			return false;
 		}
 	}
-	totalPrimes = primesArray.reduce(function(acc, value) {
-		return acc + value;
-	}, 0);
-	return totalPrimes;
+	return true;
+}
+
+function sumPrimes(num) {
+	var total = 2;
+	for (var n = 3; n <= num; n++)
+		if (isPrime(n)) {
+			total += n;
+		}
+	return total;
 }
 sumPrimes(10);
