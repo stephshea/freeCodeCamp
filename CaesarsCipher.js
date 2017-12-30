3f8af323834720ecc4adc4e207ddbacd4e1c22c4
Stephanie Shea <stephcshea@gmail.com>
2017-12-30 15:01:50 +0000

    working on Caesars Cipher

---
+++ CaesarsCipher.js
@@ -0,0 +1,18 @@
+//incomplete...
+
+function rot13(str) { // LBH QVQ VG!
+  for(i = 0; i<str.length; i++) {
+ 
+    var nStr = str.charCodeAt((i)-13);
+  //yeilds char num
+    
+     nStr.StringfromCharCode(i);
+  return nStr;
+  }
+
+
+  
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
