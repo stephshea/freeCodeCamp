435636908e7b3b640266a8831af53032b6174663
Stephanie Shea <stephcshea@gmail.com>
2017-12-31 14:34:27 +0000

    completed Ceasars Cypher

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -1,18 +1,15 @@
-//incomplete...
-
 function rot13(str) { // LBH QVQ VG!
-  for(i = 0; i<str.length; i++) {
- 
-    var nStr = str.charCodeAt((i)-13);
-  //yeilds char num
-    
-     nStr.StringfromCharCode(i);
-  return nStr;
-  }
-
-
-  
+	var winningArray = [];
+	for (var i = 0; i < str.length; i++) {
+		if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 77) {
+			winningArray.push(String.fromCharCode(str.charCodeAt(i) + 13));
+		} else if (str.charCodeAt(i) >= 78 && str.charCodeAt(i) <= 90) {
+			winningArray.push(String.fromCharCode(str.charCodeAt(i) - 13));
+		} else if (str.charCodeAt(i) < 65) {
+			winningArray.push(str[i]);
+		}
+	}
+	return winningArray.join("");
 }
-
 // Change the inputs below to test
 rot13("SERR PBQR PNZC");
