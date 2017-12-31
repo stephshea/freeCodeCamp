1c867bbc948f07e0f8675504dcc895c9c0b46035
Stephanie Shea <stephcshea@gmail.com>
2017-12-31 15:07:54 +0000

    added alternative Ceasars Cipher solution using map()

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -13,3 +13,21 @@ function rot13(str) { // LBH QVQ VG!
 }
 // Change the inputs below to test
 rot13("SERR PBQR PNZC");
+
+
+//alternative
+function rot13(str) { // LBH QVQ VG!
+	str = str.split('');
+	return str.map(function(letter) {
+		letter = letter.charCodeAt();
+		console.log(letter);
+		if (letter >= 65 && letter <= 77) {
+			letter = letter + 13;
+		} else if (letter >= 78 && letter <= 90) {
+			letter = letter - 13;
+		}
+		return String.fromCharCode(letter);
+	}).join('');
+}
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
