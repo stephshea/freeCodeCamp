89ff01587391d581d4581802f03097a8f8a7553d
Stephanie Shea <stephcshea@gmail.com>
2018-01-02 20:02:24 +0000

    a few modifications

--- sumAllNumbersInRange.js
+++ sumAllNumbersInRange.js
@@ -1,14 +1,25 @@
-function sumAll(arr) {
-	var total = 0;
-	var max = Math.max(arr[0], arr[1]);
-	var min = Math.min(arr[0], arr[1]);
-	var numbers = [];
-	for (var i = min; i <= max; i++) {
-		numbers.push(i);
-	}
-	total = numbers.reduce(function(a, b) {
-		return a + b;
-	});
-	return total;
-}
-sumAll([1, 4]);
+  function sumAll(arr) {
+  	var max = Math.max(arr[0], arr[1]);
+  	var min = Math.min(arr[0], arr[1]);
+  	var total = 0;
+  	for (var i = min; i <= max; i++) {
+  		total += i;
+  	}
+  	return total;
+  }
+  sumAll([1, 4]);
+  
+  
+  //longer code w array
+  function sumAll(arr) {
+  	var max = Math.max(arr[0], arr[1]);
+  	var min = Math.min(arr[0], arr[1]);
+  	var numbers = [];
+  	for (var i = min; i <= max; i++) {
+  		numbers.push(i);
+  	}
+  	return numbers.reduce(function(a, b) {
+  		return a + b;
+  	});
+  }
+  sumAll([1, 4]);
