03446053ca53d6203c8122e8a28fbf1f20d7cc27
Stephanie Shea <stephcshea@gmail.com>
2018-01-18 02:09:36 +0000

    added variations for Confirm the Ending

--- confirmEnding.js
+++ confirmEnding.js
@@ -6,3 +6,14 @@ function confirmEnding(str, target) {
 	return false;
 }
 confirmEnding("Bastian", "n");
+
+function confirmEnding(str, target) {
+	var start = 0 - target.length;
+	return target === str.slice(start);
+}
+confirmEnding("Bastian", "n");
+
+function confirmEnding(str, target) {
+	return target === str.slice(-Math.abs(target.length));
+}
+confirmEnding("Bastian", "n");
