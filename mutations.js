9bc48343f392d80a4887dd4853a56795693b2ebb
Stephanie Shea <stephcshea@gmail.com>
2018-01-22 01:10:32 +0000

    added solutions

--- mutations.js
+++ mutations.js
@@ -1,4 +1,16 @@
 function mutation(arr) {
+	var mainArr = arr[0].toLowerCase();
+	var smallArr = arr[1].toLowerCase();
+	for (var i = 0; i < smallArr.length; i++) {
+		if (mainArr.indexOf(smallArr[i]) === -1) {
+			return false;
+		}
+	}
+	return true;
+}
+mutation(["hello", "hey"]);
+
+function mutation(arr) {
 	var one = arr[0].toLowerCase().split("");
 	var two = arr[1].toLowerCase().split("");
 	for (var i = 0; i < two.length; i++) {
