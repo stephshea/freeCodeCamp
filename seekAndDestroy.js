9d1e837f5a108003ca4601e367810e5f97d9461c
Stephanie Shea <stephcshea@gmail.com>
2018-01-23 22:51:30 +0000

    alternate solution for Destroyer

--- seekAndDestroy.js
+++ seekAndDestroy.js
@@ -5,3 +5,16 @@ function destroyer(arr) {
 	});
 }
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+function destroyer(arr ) {
+  console.log(arguments);
+  var args = Array.prototype.slice.call(arguments);
+  console.log(args);
+  return arr.filter(function(num) {
+  return args.indexOf(num) === -1;
+
+});
+}
+//   console.log(arguments);
+//   var args = Array.from(arguments);
+//
