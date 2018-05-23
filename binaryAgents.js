c289dba2af84887808a77c7513fbfff48a7b75f4
Stephanie Shea <stephcshea@gmail.com>
2018-05-23 13:54:39 +0000

    added a one line solution

--- binaryAgents.js
+++ binaryAgents.js
@@ -1,4 +1,9 @@
 function binaryAgent(str) {
+    return String.fromCharCode.apply(null, str.split(" ").map(function(binary) {return parseInt(binary, 2);}));
+}
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+function binaryAgent(str) {
 	var binary = str.split(" ").map(function(binary) {
 		return String.fromCharCode(parseInt(binary, 2));
 	}).join('');
